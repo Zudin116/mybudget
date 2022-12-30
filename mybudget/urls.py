c7d4ccfebb8aae39ddf176a268195456b01797b6
Valentin Basin <zudin116@gmail.com>
2022-12-31 00:15:18 +0300

    start creating new html templates

--- mybudget/urls.py
+++ mybudget/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from account.views import index
+# from account.views import index
+from category.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index, name="index"),
+    path("account/", index, name="account"),
 ]
